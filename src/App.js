c3c72e37a683fc57a55c0166d7b3fe74fda61e58
Biju Nair <bnnair@yahoo.com>
2023-05-01 16:05:57 +0530

    inital commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hi There! This is <h1>BIJU NAIR</h1>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GOD HELP
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
